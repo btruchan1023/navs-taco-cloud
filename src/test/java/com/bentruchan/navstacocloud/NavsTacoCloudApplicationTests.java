90d32b37acb5a35bd710460e03b255d52d474062
btruchan1023 <33106821+btruchan1023@users.noreply.github.com>
2022-05-18 17:06:26 -0500

    Initial commit for spring in action learning project

---
+++ src/test/java/com/bentruchan/navstacocloud/NavsTacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bentruchan.navstacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavsTacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
